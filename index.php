d1d337717d73a1ecc055557a06611211d70f7b16
peter <smalld.tw@gmail.com>
2013-11-21 01:07:36 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+      <html xmlns:fb="http://www.facebook.com/2008/fbml">
+      <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+      <title>我的第一個facebook canvas app</title>
+      <!--請在此「開始」設定Open Graph Meta Tag-->
+
+
+
+
+
+      <!--請在此「結束」設定Open Graph Meta Tag-->
+		<link type="text/css" rel="stylesheet" href="css/demo.css" />
+		<link type="text/css" rel="stylesheet" href="jquery.mmenu/css/jquery.mmenu.css" />
+
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script type="text/javascript" src="jquery.mmenu/js/jquery.mmenu.js"></script>
+		<script type="text/javascript">
+			$(function() {
+				$('nav#menu').mmenu();
+			});
+		</script>
+	</head>
+	<body>
+		<div id="page">
+			<div id="header">
+				<a href="#menu"></a>
+            <!--可在此設定header標題-->
+            Open Graph 測試
+			</div>
+			<div id="content">
+         <!--請在此「開始」撰寫PHP或HTML-->
+
+
+
+
+
+         <!--請在此「結束」撰寫PHP或HTML-->
+			</div>
+
+         <?php
+            include "menu.php";
+         ?>
+		</div>
+	</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,9 @@
+			<nav id="menu">
+				<ul>
+					<li><a href="index.php">Open Graph 測試</a></li>
+               <li><a href="index2.php">Social plugin 測試</a></li>
+               <li><a href="index3.php"></a></li>
+               <li><a href="index4.php"></a></li>
+               <li><a href="index5.php"></a></li>
+				</ul>
+			</nav>
